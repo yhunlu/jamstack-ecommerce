7ee85d86535a2363643fbcf0c6df83b9118a9cca
mrsbluegreen <mrsbluegreen@hotmail.com>
2022-01-14 03:39:46 +0300

    added 3d-carousel in PromotionalProducts

--- frontend/src/components/home/PromotionalProducts.js
+++ frontend/src/components/home/PromotionalProducts.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Grid, IconButton, Typography } from "@material-ui/core"
 import { useStaticQuery, graphql } from "gatsby"
 import { makeStyles } from "@material-ui/core/styles"
@@ -19,12 +19,44 @@ const useStyles = makeStyles(theme => ({
     paddingLeft: "10rem",
     paddingRight: "10rem",
     paddingTop: "30rem",
-    paddingBottom: "30rem",
+    paddingBottom: "10rem",
+  },
+  productName: {
+    color: "#fff",
+  },
+  iconButton: {
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
+  carouselImage: {
+    height: "30rem",
+    width: "25rem",
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    boxShadow: theme.shadows[5],
+  },
+  carouselContainer: {
+    marginLeft: "20rem",
+  },
+  space: {
+    margin: "0 15rem",
+    marginBottom: "10rem",
+  },
+  explore: {
+    textTransform: "none",
+    marginRight: "2rem",
+  },
+  descriptionContainer: {
+    textAlign: "right",
   },
 }))
 
 const PromotionalProducts = () => {
   const classes = useStyles()
+
+  const [selectedSlide, setSelectedSlide] = useState(0)
+
   const data = useStaticQuery(graphql`
     query GetPromo {
       allStrapiProduct(filter: { promo: { eq: true } }) {
@@ -44,6 +76,45 @@ const PromotionalProducts = () => {
     }
   `)
 
+  var slides = []
+
+  data.allStrapiProduct.edges.map(({ node }, i) =>
+    slides.push({
+      key: i,
+      content: (
+        <Grid container direction="column" alignItems="center">
+          <Grid item>
+            <IconButton
+              disableRipple
+              onClick={() => setSelectedSlide(i)}
+              classes={{
+                root: clsx(classes.iconButton, {
+                  [classes.space]: selectedSlide !== i,
+                }),
+              }}
+            >
+              <img
+                src={
+                  process.env.GATSBY_STRAPI_URL + node.variants[0].images[0].url
+                }
+                alt={`image-${i}`}
+                className={classes.carouselImage}
+              />
+            </IconButton>
+          </Grid>
+          <Grid item>
+            {selectedSlide === i ? (
+              <Typography variant="h1" classes={{ root: classes.productName }}>
+                {node.name.split(" ")[0]}
+              </Typography>
+            ) : null}
+          </Grid>
+        </Grid>
+      ),
+      description: node.description,
+    })
+  )
+
   return (
     <Grid
       container
@@ -51,8 +122,20 @@ const PromotionalProducts = () => {
       alignItems="center"
       classes={{ root: classes.mainContainer }}
     >
-      <Grid item>Carousel</Grid>
-      <Grid item>Description</Grid>
+      <Grid item classes={{ root: classes.carouselContainer }}>
+        <Carousel slides={slides} goToSlide={selectedSlide} />
+      </Grid>
+      <Grid item classes={{ root: classes.descriptionContainer }}>
+        <Typography variant="h2" paragraph>
+          {slides[selectedSlide].description}
+        </Typography>
+        <Button>
+          <Typography variant="h4" classes={{ root: classes.explore }}>
+            Explore
+          </Typography>
+          <img src={explore} alt="go to product page" />
+        </Button>
+      </Grid>
     </Grid>
   )
 }
